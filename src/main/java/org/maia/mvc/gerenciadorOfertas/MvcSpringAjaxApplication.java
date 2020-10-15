aa019b0f9e60113aca97dfba12bf2190265070c2
DOWGLAS <dowglasmaia@live.com>
2020-10-15 16:42:58 -0300

    initial config project

---
+++ src/main/java/org/maia/mvc/gerenciadorOfertas/MvcSpringAjaxApplication.java
@@ -0,0 +1,13 @@
+package org.maia.mvc.gerenciadorOfertas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSpringAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcSpringAjaxApplication.class, args);
+	}
+
+}
