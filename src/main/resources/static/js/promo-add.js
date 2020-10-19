28e97845675be9327668821101db46e45103b9f8
DOWGLAS <dowglasmaia@live.com>
2020-10-19 11:48:34 -0300

    impl load anametion

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -14,33 +14,41 @@ $("#linkPromocao").on('change', function () {
 			  $("#alert").removeClass("alert alert-danger").text(" ");//removendo msg de error
 			  $("#titulo").val(""); // limpa o campo de titulo
 			  $("#site").text(""); // limpa o campo  onde estar o nome do dite
-			  $("#linkImagem").attr("src","/images/promo-dark.png "); // limpa o campo onde estar o nome da imagem
+			  $("#linkImagem").attr("src"," "); // limpa o campo onde estar o nome da imagem
+			  $("#loader-img").addClass("loader");
 		  },		  
 		  success: function( data ){
 			  console.log(data);
 			  $("#titulo").val(data.title);
-			  $("#site").text(data.site.replace("@",""));
-			  $("#linkImagem").attr("src",data.image);			  
-		  },		  
+			  $("#site").text(data.site.replace("@",""));			 
+			  $("#linkImagem").attr("src",data.image);	
+			},		  
 		  statusCode: {
 			  /*criando messagem de resposta de Error*/
 			  404: function(){
 				  $("#alert")
 				  .addClass("alert alert-danger")
 				  .text( "Nenhuma informação pode ser recuperada dessa url!");
-			  },
+				  $("#linkImagem").attr("src","/images/promo-dark.png "); // limpa o campo onde estar o nome da imagem
+			},
 			  500: function(){
 				  $("#alert")
 				  .addClass("alert alert-danger")
 				  .text( "Url inválida");
-			  }	
+				  $("#linkImagem").attr("src","/images/promo-dark.png "); // limpa o campo onde estar o nome da imagem
+				 }	
 		  },
 		  /*Erros genericos*/
 		  error: function(){
 			  $("#alert")
 			  .addClass("alert alert-danger")
 			  .text( "Ops... algo deu errado, tente novamente mas tarde");
-		  }
+			  $("#linkImagem").attr("src","/images/promo-dark.png "); // limpa o campo onde estar o nome da imagem
+			},
+			/*apois concluir a operação*/
+		 complete: function(){
+			 $("#loader-img").removeClass("loader");
+		 },
 	  })
   }
   
