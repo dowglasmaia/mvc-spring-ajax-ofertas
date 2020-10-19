96a09ba13eaf7b6a7534481b98d9cde9148b3ff0
DOWGLAS <dowglasmaia@live.com>
2020-10-19 11:24:03 -0300

    validações antes das requisições

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -9,12 +9,19 @@ $("#linkPromocao").on('change', function () {
 		  method:"POST",
 		  url:"/meta/info?url=" + url,
 		  cache: false,
+		  //antes  de qualquer requisição
+		  beforeSend: function(){
+			  $("#alert").removeClass("alert alert-danger").text(" ");//removendo msg de error
+			  $("#titulo").val(""); // limpa o campo de titulo
+			  $("#site").text(""); // limpa o campo  onde estar o nome do dite
+			  $("#linkImagem").attr("src","/images/promo-dark.png "); // limpa o campo onde estar o nome da imagem
+		  },		  
 		  success: function( data ){
 			  console.log(data);
 			  $("#titulo").val(data.title);
 			  $("#site").text(data.site.replace("@",""));
 			  $("#linkImagem").attr("src",data.image);			  
-		  },
+		  },		  
 		  statusCode: {
 			  /*criando messagem de resposta de Error*/
 			  404: function(){
