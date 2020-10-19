81fc8bb4ef8434ed6bf46133af85d7c8d547e7ed
DOWGLAS <dowglasmaia@live.com>
2020-10-19 13:43:35 -0300

    Criando class de promocao

---
+++ src/main/java/org/maia/mvc/gerenciadorOfertas/domain/Promocao.java
@@ -0,0 +1,71 @@
+package org.maia.mvc.gerenciadorOfertas.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+import groovy.transform.builder.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@SuppressWarnings("serial")
+@Data
+@Builder
+@AllArgsConstructor
+//@NoArgsConstructor
+@Entity
+@Table
+public class Promocao implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Informe o Nome do site da Promoção")
+	@Column(name = "site_promocao", nullable = false)
+	private String site;
+
+	@NotBlank(message = "Informe o Titulo da Promoção")
+	@Column(name = "title_promocao", nullable = false)
+	private String title;
+
+	@NotBlank(message = "Informe o Link da Promoção")
+	@Column(name = "link_promocao", nullable = false)
+	private String linkPromocao;
+
+	@Column(name = "descricao", nullable = false)
+	private String descricao;
+
+	@NotNull(message = "Informe o Preço da Promoção")
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+	@Column(name = "preco_promocao", nullable = false)
+	private BigDecimal preco;
+
+	@NotBlank(message = "Informe o endereço(url) da Imagem da Promoção")
+	@Column(name = "link_imagem", nullable = false)
+	private String linkImage;
+
+	@Column(name = "total_links")
+	private String likes;
+
+	@Column(name = "data_cadastro")
+	private LocalDateTime dtaCadastroDateTime;
+
+	public Promocao() {
+		this.dtaCadastroDateTime = LocalDateTime.now();
+	}
+
+}
