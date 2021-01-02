bf2d55a1e5a16aa771a7182a465134514d6dd606
Dowglas Maia <dowglasmaia@gmail.com>
2021-01-02 13:05:55 -0300

    Criando as Funções para buscar o likes por id e update de Likes para promoção

--- src/main/java/org/maia/mvc/gerenciadorOfertas/repository/PromocaoRepo.java
+++ src/main/java/org/maia/mvc/gerenciadorOfertas/repository/PromocaoRepo.java
@@ -2,9 +2,22 @@ package org.maia.mvc.gerenciadorOfertas.repository;
 
 import org.maia.mvc.gerenciadorOfertas.domain.Promocao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PromocaoRepo extends JpaRepository<Promocao, Long> {
+	
+	
+	@Transactional(readOnly = false) // modificando a transação para poder realizar o update
+	@Modifying
+	@Query("update Promocao p set p.likes = p.likes + 1 where p.id = :id") //atualizando o valor das curtidas
+	void updateSomaLike(@Param("id") Long id);
+	
+	@Query("select p.likes from Promocao p where p.id = :id")
+	int findLinkesById(@Param("id") Long id);
 
 }
